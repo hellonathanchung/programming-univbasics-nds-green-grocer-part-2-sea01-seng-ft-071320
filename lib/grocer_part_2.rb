2806388c654004d72b36aa9c46275f0b992f5554
Nathan Chung <chung.nathanh@gmail.com>
2020-07-07 19:06:21 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,29 +1,21 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  counter = 0
-  while counter < coupons.length 
-    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
-    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
-    
-  if cart_item && cart_item[:count]  >= coupons[:counter][:num]
-    if cart_item_with_coupon
-      cart_item_with_coupon[:count] += coupons[counter][:num]
-      cart_item[:count]-= coupons[counter][:num]
-    else 
-      cart_item_with_coupon = {
-      :item => couponed_item_name,
-      :price => coupons[counter][:cost] / coupons[counter][:num],
-      :count => coupons[counter][:num],
-      :clearance => cart_item[:clearance],
-      }
-      cart << cart_item_with_coupon
-      cart_item[:count] -= coupons[counter][:num]
-      end 
-   end
-    counter += 1 
-  end 
+  i = 0
+  coupons.each do |coupon|
+    item_with_coupon = find_item_by_name_in_collection(coupon[:item], cart)
+    item_is_in_basket = !!item_with_coupon
+    count_is_big_enough_to_apply = item_is_in_basket && item_with_coupon[:count] >= coupon[:num]
+    if item_is_in_basket and count_is_big_enough_to_apply
+      cart << { item: "#{item_with_coupon[:item]} W/COUPON", 
+                price: coupon[:cost] / coupon[:num], 
+                clearance: item_with_coupon[:clearance],
+                count: coupon[:num]
+              }
+      item_with_coupon[:count] -= coupon[:num]
+    end
+    i += 1
+  end
   cart
 end
 
